1f5b440941f59dc9c82777a13a6c4dec6285a322
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-23 17:20:13 -0300

    style: Header and Footer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
+import styles from './Header.module.css';
+
 function Header() {
   return (
-    <header>
+    <header className={ styles.header }>
       <h1>Todo React + Typescript</h1>
     </header>
   );

789c316875f7a27eb5af4ce3f16819b5095016c7
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-09-07 11:31:28 -0300

    feat: component TaskList

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,10 +1,32 @@
+import style from './TaskList.module.css';
 
-type Props = {}
+import { ITask } from "../../interface/ITask";
 
-const TaskList = (props: Props) => {
+type Props = {
+  taskList: ITask[];
+};
+
+const TaskList = ({ taskList }: Props) => {
   return (
-    <div>TaskList</div>
-  )
-}
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id} className={ style.task }>
+            <div className={ style.details}>
+              <h4>{task.title}</h4>
+              <p>Dificuldade:{task.difficulty}</p>
+            </div>
+            <div className={ style.actions }>
+              <i className="bi bi-pencil"></i>
+              <i className="bi bi-trash"></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas!</p>
+      )}
+    </>
+  );
+};
 
 export default TaskList;

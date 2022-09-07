d9112c7d4084848702a8988630b764edc95a080e
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-09-07 10:41:18 -0300

    feat: send the props to taskForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,38 @@
 // components
-import Header from './components/Header';
-import Footer from './components/Footer';
-import TaskForm from './components/TaskForm';
-import TaskList from './components/TaskList';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 
 // interface
-import { ITask } from './interface/ITask';
+import { useState } from "react";
 
-import styles from './App.module.css';
-import { useState } from 'react';
+import styles from "./App.module.css";
+
+// interface
+import { ITask } from "./interface/ITask";
 
 function App() {
-  const [taskList, set] = useState<ITask[]>([]);
+  const [taskList, setTaskList] = useState<ITask[]>([]);
 
   return (
     <div>
-      <Header/>
-      <main className={ styles.main }>
+      <Header />
+      <main className={styles.main}>
         <div>
           <h2>O que você vai fazer?</h2>
-          <TaskForm btnText='Criar tarefa'/>  
+          <TaskForm
+            btnText="Criar tarefa"
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
         </div>
         <div>
           <h2>Lista de Tarefas:</h2>
           <TaskList />
         </div>
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
